d922d74adc6dfccd3b4a9c08b5d717ee4d0fc7c8
mukultwr <twrmukul0101@gmail.com>
2022-06-02 13:06:35 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import "./utils/routes.dart";
+import './pages/login.dart';
+import './pages/otp.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+     routes: {
+        "/": (context) => LoginPage(),
+        MyRoutes.otp:((context) => Otp()),
+        MyRoutes.loginRoute:(context) => LoginPage()
+      },
+    );
+
+   
+  }
+}
