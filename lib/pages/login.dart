4d05b12a1b44b78cd175e6657a16de6f8fe808ab
chirayu-47 <ck8161@gmail.com>
2022-07-05 15:23:52 +0530

    UikDivider added

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:login/pages/UikDivider/UikDivider.dart';
+import 'package:login/widgets/UikDivider/UikDivider.dart';
 import 'package:login/pages/otp.dart';
 import 'package:login/widgets/UikAvatar/UikAvatar.dart';
 import 'package:login/widgets/UikButton/UikButton.dart';
