2fbcacb247a4f530af072d888469719003b784f5
prashantdevjengaria <37360561+prashantdevjengaria@users.noreply.github.com>
2022-07-15 20:21:10 +0530

    Update uikText.dart

--- lib/uikText.dart
+++ lib/uikText.dart
@@ -5,13 +5,14 @@ class UikText extends StatelessWidget {
   final double size;
   final String text;
   final FontWeight weight;
+  final Color color;
 
-  UikText({required this.size, required this.text, required this.weight});
+  UikText({required this.size, required this.text, required this.weight, required this.color});
 
   @override
   Widget build(BuildContext context) {
     return Text(text,
-    style: GoogleFonts.poppins(fontSize: size, fontWeight: weight),
+    style: GoogleFonts.poppins(fontSize: size, fontWeight: weight, color: color),
     );
   }
 }
