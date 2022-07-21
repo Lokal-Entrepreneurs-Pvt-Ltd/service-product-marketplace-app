7e45492aab2110f455c450d3404a77fd827e6b62
prashantdevjengaria <prashantdevjengaria@gmail.com>
2022-07-21 15:06:40 +0530

    Update uikText.dart

--- lib/uikText.dart
+++ lib/uikText.dart
@@ -2,17 +2,38 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class UikText extends StatelessWidget {
-  final double size;
   final String text;
+  final double size;
   final FontWeight weight;
   final Color color;
+  final double wordSpacing;
+  final Color backgroundColor;
+  final Color decorationColor;
+  final FontStyle fontStyle;
 
-  UikText({required this.size, required this.text, required this.weight, required this.color});
+ UikText({
+    required this.text,
+    this.size = 18,
+    this.weight = FontWeight.w400,
+    this.color = Colors.black,
+  this.wordSpacing = 1.15,
+   this.backgroundColor = Colors.transparent,
+   this.decorationColor = Colors.transparent,
+   this.fontStyle = FontStyle.normal,
+ });
 
   @override
   Widget build(BuildContext context) {
     return Text(text,
-    style: GoogleFonts.poppins(fontSize: size, fontWeight: weight, color: color),
+    style: GoogleFonts.poppins(
+        fontSize: size,
+        fontWeight: weight,
+        color: color,
+    wordSpacing: wordSpacing,
+    backgroundColor: backgroundColor,
+    decorationColor: decorationColor,
+    fontStyle: fontStyle,
+    ),
     );
   }
 }
