3a69073cf656f1b99e333fea376adaf349bd3603
Mano Vikram <manovik18@gmail.com>
2022-11-26 20:56:38 +0530

    Made minor changes

--- lib/screens/Login/login.dart
+++ lib/screens/Login/login.dart
@@ -4,7 +4,7 @@ import 'package:login/Widgets/UikTextField/UikTextField.dart';
 import 'package:login/pages/UikBottomNavigationBar.dart';
 import 'package:login/widgets/UikButton/UikButton.dart';
 
-import '../../Widgets/UikNavbar/UikNavbar.dart';
+import '../../widgets/UikNavbar/UikNavbar.dart';
 
 class LoginPageScreen extends StatefulWidget {
   const LoginPageScreen({super.key});
