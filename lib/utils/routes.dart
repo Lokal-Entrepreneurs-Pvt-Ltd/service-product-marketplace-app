a3fa3ca2db8b2efcbdb083aa765c7f5b2e317364
Mano Vikram <manovik18@gmail.com>
2022-12-04 17:45:40 +0530

    Added features to report crash with firebase! There is some bug - the crash logs are not being shown on the firebase console, gotta resolve it!

--- lib/utils/routes.dart
+++ lib/utils/routes.dart
@@ -1,11 +1,13 @@
 class MyRoutes {
-  static String loginRoute = '/login';
+  static String loginRoute = '/loginScreen';
   static String homeRoute = '/home';
   static String productsCatalogue = "/productsCatalogue";
+  static String searchCatalogue = "/searchCatalogue";
+  static String productScreen = "/productScreen";
   static String orderRoute = '/order';
   static String filterRoute = '/filter';
   static String cartRoute = '/cart';
-  static String otp = "/otp";
+  static String otp = "/otpScreen";
   static String avatar = "/avatar";
   static String icon = "/icon";
 }
