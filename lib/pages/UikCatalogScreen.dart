c80abb7064ae4ea9ad452250ebe3dee3386ff074
Mano Vikram <manovik18@gmail.com>
2022-12-09 20:14:19 +0530

    Changed SDK version to 24 and replaced the broken CatalogueScreen API endpoint!

--- lib/pages/UikCatalogScreen.dart
+++ lib/pages/UikCatalogScreen.dart
@@ -34,19 +34,16 @@ class UikCatalogScreen extends StandardPage {
 }
 
 Future<StandardScreenResponse> fetchAlbum() async {
-  print("entering");
-  final response = await http.post(
-    Uri.parse('http://localhost:3000/discovery/get'),
+  final response = await http.get(
+    Uri.parse('https://demo1595178.mockable.io/CatalogScreen'),
     headers: {
       "ngrok-skip-browser-warning": "value",
-      //"id" : "eb5f37b2-ca34-40a1-83ba-cb161eb55e6e",
-      //"token" : "h45ngvJIR7PjW-MXpLaUWlKdrwk3CNjerz9U1QnK1AA.eyJpbnN0YW5jZUlkIjoiZGQ2YjVjMDEtNWNlNC00ZTc1LWE1MmUtOWM0YmM1Zjc4ZjI2IiwiYXBwRGVmSWQiOiIyMmJlZjM0NS0zYzViLTRjMTgtYjc4Mi03NGQ0MDg1MTEyZmYiLCJtZXRhU2l0ZUlkIjoiZGQ2YjVjMDEtNWNlNC00ZTc1LWE1MmUtOWM0YmM1Zjc4ZjI2Iiwic2lnbkRhdGUiOiIyMDIyLTA5LTE0VDExOjM0OjQ0Ljg4MloiLCJ1aWQiOiJlNmRiNzUwMC05Zjc1LTQwOTUtODllNC02MTVlY2I4OTFmNzgiLCJwZXJtaXNzaW9ucyI6Ik9XTkVSIiwiZGVtb01vZGUiOmZhbHNlLCJzaXRlT3duZXJJZCI6ImU2ZGI3NTAwLTlmNzUtNDA5NS04OWU0LTYxNWVjYjg5MWY3OCIsInNpdGVNZW1iZXJJZCI6ImU2ZGI3NTAwLTlmNzUtNDA5NS04OWU0LTYxNWVjYjg5MWY3OCIsImV4cGlyYXRpb25EYXRlIjoiMjAyMi0wOS0xNFQxNTozNDo0NC44ODJaIiwibG9naW5BY2NvdW50SWQiOiJlNmRiNzUwMC05Zjc1LTQwOTUtODllNC02MTVlY2I4OTFmNzgifQ"
     },
   );
-  dynamic res = json.decode(response.body);
-  print(response.body);
+
+  // StandardScreenResponse
   if (response.statusCode == 200) {
-    return StandardScreenResponse.fromJson(res['data']['response']);
+    return StandardScreenResponse.fromJson(jsonDecode(response.body));
   } else {
     throw Exception('Failed to load album');
   }
@@ -62,3 +59,19 @@ Future<StandardScreenResponse> fetchAlbum() async {
 
   // return StandardScreenResponse.fromJson(response.data);
 }
+
+/* Future<StandardScreenResponse> fetchAlbum() async {
+  final response = await http.get(
+    Uri.parse('https://demo1595178.mockable.io/CatalogScreen'),
+    headers: {
+      "ngrok-skip-browser-warning": "value",
+    },
+  );
+
+  // StandardScreenResponse
+  if (response.statusCode == 200) {
+    return StandardScreenResponse.fromJson(jsonDecode(response.body));
+  } else {
+    throw Exception('Failed to load album');
+  }
+} */
