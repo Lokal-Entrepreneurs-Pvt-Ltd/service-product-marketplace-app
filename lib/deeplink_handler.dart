8f3362e9126ac83e38c313764c0199fdbc9e683a
Mano Vikram <manovik18@gmail.com>
2022-12-20 22:31:26 +0530

    Added openPage func which will work for the updated url of format - lokal://productscreen?categoryid=1&productid=1

--- lib/deeplink_handler.dart
+++ lib/deeplink_handler.dart
@@ -2,7 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:lokal/utils/routes.dart';
 
 abstract class DeeplinkHandler {
-  void openDepelink(BuildContext context, String url) {
+  void openDeeplink(BuildContext context, String url) {
+    // Url formatl -> https://localee.co.in/productscreen?categoryid=1&productid=1
     String baseUrl = "";
     String route = "";
     Map<String, String> args = {};
@@ -29,6 +30,68 @@ abstract class DeeplinkHandler {
     }
 
     print(baseUrl);
+    print(route);
+    print(args);
+
+    switch (route) {
+      case MyRoutes.loginScreen:
+        {
+          if (args.isEmpty) {
+            _pushScreen(context, MyRoutes.loginScreen);
+          }
+        }
+        break;
+      case MyRoutes.homeScreen:
+        {
+          if (args.isEmpty) {
+            _pushScreen(context, MyRoutes.loginScreen);
+          }
+        }
+        break;
+      case MyRoutes.productsCatalogueScreen:
+        {
+          if (args["catalogueId"] != null) {
+            _pushScreen(context, MyRoutes.loginScreen);
+          }
+        }
+        break;
+      case MyRoutes.searchCatalogueScreen:
+        {
+          if (args.isEmpty) {
+            _pushScreen(context, MyRoutes.loginScreen);
+          }
+        }
+        break;
+      case MyRoutes.productScreen:
+        {
+          if (args["productId"] != null) {
+            _pushScreen(context, MyRoutes.loginScreen);
+          }
+        }
+        break;
+      default:
+    }
+  }
+
+  static void openPage(BuildContext context, String url) {
+    // Url formatl -> lokal://productscreen?categoryid=1&productid=1
+    String route = "";
+    Map<String, String> args = {};
+
+    List<String> urlSplits = url.split(RegExp(r'/'));
+
+    List<String> routeAndParams = urlSplits[2].split(RegExp(r'[?|&]'));
+
+    route = "/${routeAndParams[0]}";
+
+    if (routeAndParams.length > 1) {
+      for (int i = 1; i < routeAndParams.length; i++) {
+        List<String> keyValuePair = routeAndParams[i].split(RegExp(r'='));
+
+        args[keyValuePair[0]] = keyValuePair[1];
+      }
+    }
+
     print(route);
     print(args);
 
