74b9f5108c07c6ff6e0d18cc10648ee627258848
Rohitp2020 <rohitpunetha2001@gmail.com>
2023-01-15 18:45:58 +0530

    uiUtils is added for toast on my account screen.

--- lib/utils/uiUtils/uiUtils.dart
+++ lib/utils/uiUtils/uiUtils.dart
@@ -1,11 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 
-// void main() {
-//   runApp(const Toast1());
-// }
-
-class MyStaticClass {
+class uiUtils {
   static void showToast(String message) {
     Fluttertoast.showToast(
         msg: message,
@@ -17,66 +13,3 @@ class MyStaticClass {
         fontSize: 16.0);
   }
 }
-
-// static class Toast1 extends StatelessWidget {
-//   const Toast1({Key? key}) : super(key: key);
-
-// // This widget is the
-// // root of your application.
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//         home: Center(
-//       child: TextButton(
-//         onPressed: () {
-//           Fluttertoast.showToast(
-//             msg: 'Hello',
-//             backgroundColor: Colors.grey,
-//           );
-//         },
-//         child: Container(
-//           padding: const EdgeInsets.all(14),
-//           color: Colors.green,
-//           child: const Text(
-//             'Show',
-//             style: TextStyle(color: Colors.white),
-//           ),
-//         ),
-//       ),
-//     ));
-//   }
-// }
-
-// class MyHomePage extends StatefulWidget {
-//   const MyHomePage({Key? key}) : super(key: key);
-
-//   @override
-// // ignore: library_private_types_in_public_api
-//   _MyHomePageState createState() => _MyHomePageState();
-// }
-
-// class _MyHomePageState extends State<MyHomePage> {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       body: Center(
-//         child: TextButton(
-//           onPressed: () {
-//             Fluttertoast.showToast(
-//               msg: 'Hello',
-//               backgroundColor: Colors.grey,
-//             );
-//           },
-//           child: Container(
-//             padding: const EdgeInsets.all(14),
-//             color: Colors.green,
-//             child: const Text(
-//               'Show',
-//               style: TextStyle(color: Colors.white),
-//             ),
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-// }
