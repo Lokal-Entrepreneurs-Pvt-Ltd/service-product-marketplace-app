51d9b2c232b0f879d84000e08f8372cd45ec0562
Rohitp2020 <rohitpunetha2001@gmail.com>
2023-01-16 16:24:16 +0530

    Order success done

--- lib/screens/orderSuccess/orderSuccess.dart
+++ lib/screens/orderSuccess/orderSuccess.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class orderScreen extends StatelessWidget {
+class orderSuccessScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
