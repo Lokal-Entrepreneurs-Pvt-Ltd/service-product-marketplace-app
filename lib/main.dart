340902e9da53e8bd23e64de70cb97a69fe939830
Mano Vikram <manovik18@gmail.com>
2023-02-22 19:17:38 +0530

    Changed route in main.dart!

--- lib/main.dart
+++ lib/main.dart
@@ -144,8 +144,8 @@ class _LokalAppState extends State<LokalApp> {
 
           // "/": (context) => UikHome().page,
 
-          // "/": (context) => UserDataHandler.getUserToken().isEmpty ?  OnboardingScreen() : UikBottomNavigationBar(),
-          "/": (context) => UikAddAddressScreen().page,
+          "/": (context) => UserDataHandler.getUserToken().isEmpty ?  OnboardingScreen() : UikBottomNavigationBar(),
+          // "/": (context) => UikAddAddressScreen().page,
           ScreenRoutes.homeScreen: (context) => const UikHomeWrapper(),
           ScreenRoutes.catalogueScreen: (context) => UikCatalogScreen().page,
           ScreenRoutes.productScreen: (context) => UikProductPage().page,
