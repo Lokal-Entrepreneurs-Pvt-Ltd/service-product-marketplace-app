c7c60405614920a88c0c0461002eee01127600dc
Anupam Singh <anupamsingh@Anupams-MacBook-Pro.local>
2023-03-07 14:25:08 +0530

    added my games

--- lib/main.dart
+++ lib/main.dart
@@ -9,10 +9,12 @@ import 'package:lokal/pages/UikAddressBook.dart';
 import 'package:lokal/pages/UikCartScreen.dart';
 import 'package:lokal/pages/UikHome.dart';
 import 'package:lokal/pages/UikMyAccountScreen.dart';
+import 'package:lokal/pages/UikMyGames.dart';
 import 'package:lokal/pages/UikOrderHistoryScreen.dart';
 import 'package:lokal/pages/UikOrderScreen.dart';
 import 'package:lokal/pages/UikPaymentDetailsScreen.dart';
 import 'package:lokal/pages/UikInvite.dart';
+import 'package:lokal/pages/UikMyGames.dart';
 import 'package:lokal/pages/UikService.dart';
 import 'package:lokal/screens/Onboarding/OnboardingScreen.dart';
 import 'package:lokal/screens/Otp/OtpScreen.dart';
@@ -160,8 +162,8 @@ class _LokalAppState extends State<LokalApp> {
           ScreenRoutes.paymentDetailsScreen: (context) =>  UikPaymentDetailsScreen().page,
           ScreenRoutes.orderScreen: (context) =>  UikOrderScreen().page,
           ScreenRoutes.addAddressScreen: (context) =>  UikAddAddressScreen().page,
-          ScreenRoutes.myDetailsScreen: (context) =>  MyDetailsScreen(),
           ScreenRoutes.orderHistoryScreen: (context) =>  UikOrderHistoryScreen().page,
+          ScreenRoutes.myGames: (context) =>  UikMyGames().page,
           // "/": (context) => UikServiceScreen().page,
           //    ScreenRoutes.searchScreen: (context) => UikSearchCatalog().page,
           // MyApiRoutes.searchScreen: (context) => UikSearchCatalog().page,
--- lib/pages/UikPrice.dart
+++ lib/pages/UikPrice.dart
@@ -1,63 +1,62 @@
-import 'dart:convert';
-
-import 'package:lokal/actions.dart';
-import 'package:lokal/pages/UikCatalogScreen.dart';
-import 'package:ui_sdk/StandardPage.dart';
-import 'package:ui_sdk/props/ApiResponse.dart';
-import 'package:ui_sdk/props/StandardScreenResponse.dart';
-import 'package:http/http.dart' as http;
-import 'package:dio/dio.dart';
-
-import '../utils/network/retrofit/api_client.dart';
-
-// uik cart, cart screen and price check their is same or not
-
-class UikPrice extends StandardPage {
-  @override
-  Set<String?> getActions() {
-    Set<String?> actionList = Set();
-    actionList.add(UIK_ACTION.OPEN_WEB);
-    actionList.add(UIK_ACTION.OPEN_HALA);
-    return actionList;
-  }
-
-  @override
-  Future<ApiResponse> getData() {
-    return StandardScreenClient(
-            Dio(BaseOptions(contentType: "application/json")))
-        .getHomeScreen("");
-  }
-
-  @override
-  getPageCallBackForAction() {
-    return of;
-  }
-
-  void of() {}
-
-  @override
-  getPageContext() {
-    return UikPrice;
-  }
-}
-
-Future<StandardScreenResponse> getMockedApiResponse() async {
-  final queryParameter = {
-    "id": "eb5f37b2-ca34-40a1-83ba-cb161eb55e6e",
-  };
-  final response = await http.get(
-    Uri.parse('https://8969-1-39-200-14.in.ngrok.io/getcart'),
-    headers: {
-      "ngrok-skip-browser-warning": "value",
-      "id": "34f6221f-595a-474f-95a8-e6fe491ec62a",
-      "token":
-          "wKvzk08wyJdkgM-k6CfkrXzRmLPsWGlRAiCwort1pRw.eyJpbnN0YW5jZUlkIjoiZGQ2YjVjMDEtNWNlNC00ZTc1LWE1MmUtOWM0YmM1Zjc4ZjI2IiwiYXBwRGVmSWQiOiIyMmJlZjM0NS0zYzViLTRjMTgtYjc4Mi03NGQ0MDg1MTEyZmYiLCJtZXRhU2l0ZUlkIjoiZGQ2YjVjMDEtNWNlNC00ZTc1LWE1MmUtOWM0YmM1Zjc4ZjI2Iiwic2lnbkRhdGUiOiIyMDIyLTA5LTE1VDE0OjE1OjM3Ljg5OFoiLCJ1aWQiOiJlNmRiNzUwMC05Zjc1LTQwOTUtODllNC02MTVlY2I4OTFmNzgiLCJwZXJtaXNzaW9ucyI6Ik9XTkVSIiwiZGVtb01vZGUiOmZhbHNlLCJzaXRlT3duZXJJZCI6ImU2ZGI3NTAwLTlmNzUtNDA5NS04OWU0LTYxNWVjYjg5MWY3OCIsInNpdGVNZW1iZXJJZCI6ImU2ZGI3NTAwLTlmNzUtNDA5NS04OWU0LTYxNWVjYjg5MWY3OCIsImV4cGlyYXRpb25EYXRlIjoiMjAyMi0wOS0xNVQxODoxNTozNy44OThaIiwibG9naW5BY2NvdW50SWQiOiJlNmRiNzUwMC05Zjc1LTQwOTUtODllNC02MTVlY2I4OTFmNzgifQ"
-    },
-  );
-
-  if (response.statusCode == 200) {
-    return StandardScreenResponse.fromJson(jsonDecode(response.body));
-  } else {
-    throw Exception('Failed to load album');
-  }
-}
+// import 'dart:convert';
+//
+// import 'package:lokal/actions.dart';
+// import 'package:lokal/pages/UikCatalogScreen.dart';
+// import 'package:ui_sdk/StandardPage.dart';
+// import 'package:ui_sdk/props/ApiResponse.dart';
+// import 'package:ui_sdk/props/StandardScreenResponse.dart';
+// import 'package:http/http.dart' as http;
+// import 'package:dio/dio.dart';
+//
+//
+// // uik cart, cart screen and price check their is same or not
+//
+// class UikPrice extends StandardPage {
+//   @override
+//   Set<String?> getActions() {
+//     Set<String?> actionList = Set();
+//     actionList.add(UIK_ACTION.OPEN_WEB);
+//     actionList.add(UIK_ACTION.OPEN_HALA);
+//     return actionList;
+//   }
+//
+//   @override
+//   Future<ApiResponse> getData() {
+//     return StandardScreenClient(
+//             Dio(BaseOptions(contentType: "application/json")))
+//         .getHomeScreen("");
+//   }
+//
+//   @override
+//   getPageCallBackForAction() {
+//     return of;
+//   }
+//
+//   void of() {}
+//
+//   @override
+//   getPageContext() {
+//     return UikPrice;
+//   }
+// }
+//
+// Future<StandardScreenResponse> getMockedApiResponse() async {
+//   final queryParameter = {
+//     "id": "eb5f37b2-ca34-40a1-83ba-cb161eb55e6e",
+//   };
+//   final response = await http.get(
+//     Uri.parse('https://8969-1-39-200-14.in.ngrok.io/getcart'),
+//     headers: {
+//       "ngrok-skip-browser-warning": "value",
+//       "id": "34f6221f-595a-474f-95a8-e6fe491ec62a",
+//       "token":
+//           "wKvzk08wyJdkgM-k6CfkrXzRmLPsWGlRAiCwort1pRw.eyJpbnN0YW5jZUlkIjoiZGQ2YjVjMDEtNWNlNC00ZTc1LWE1MmUtOWM0YmM1Zjc4ZjI2IiwiYXBwRGVmSWQiOiIyMmJlZjM0NS0zYzViLTRjMTgtYjc4Mi03NGQ0MDg1MTEyZmYiLCJtZXRhU2l0ZUlkIjoiZGQ2YjVjMDEtNWNlNC00ZTc1LWE1MmUtOWM0YmM1Zjc4ZjI2Iiwic2lnbkRhdGUiOiIyMDIyLTA5LTE1VDE0OjE1OjM3Ljg5OFoiLCJ1aWQiOiJlNmRiNzUwMC05Zjc1LTQwOTUtODllNC02MTVlY2I4OTFmNzgiLCJwZXJtaXNzaW9ucyI6Ik9XTkVSIiwiZGVtb01vZGUiOmZhbHNlLCJzaXRlT3duZXJJZCI6ImU2ZGI3NTAwLTlmNzUtNDA5NS04OWU0LTYxNWVjYjg5MWY3OCIsInNpdGVNZW1iZXJJZCI6ImU2ZGI3NTAwLTlmNzUtNDA5NS04OWU0LTYxNWVjYjg5MWY3OCIsImV4cGlyYXRpb25EYXRlIjoiMjAyMi0wOS0xNVQxODoxNTozNy44OThaIiwibG9naW5BY2NvdW50SWQiOiJlNmRiNzUwMC05Zjc1LTQwOTUtODllNC02MTVlY2I4OTFmNzgifQ"
+//     },
+//   );
+//
+//   if (response.statusCode == 200) {
+//     return StandardScreenResponse.fromJson(jsonDecode(response.body));
+//   } else {
+//     throw Exception('Failed to load album');
+//   }
+// }
