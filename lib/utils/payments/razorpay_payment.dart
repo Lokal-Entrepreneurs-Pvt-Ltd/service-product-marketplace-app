b47f6c2e40ff1d6bd50eec1d6094952fac88872f
Mano Vikram <manovik18@gmail.com>
2023-03-13 15:15:15 +0530

    Razorpay integrated but not working properly!

--- lib/utils/payments/razorpay_payment.dart
+++ lib/utils/payments/razorpay_payment.dart
@@ -1,25 +1,31 @@
+import 'package:lokal/utils/storage/user_data_handler.dart';
 import 'package:razorpay_flutter/razorpay_flutter.dart';
 
 class RazorpayPayment {
   final Razorpay _razorpay = Razorpay();
 
-  Map<String, dynamic> options = {
-    'key': 'HqtzkMv1vxs2f8',
-    'amount': 0, //in the smallest currency sub-unit.
-    'name': 'Lokal',
-    'order_id': '', // Generate order_id using Orders API
-    'description': 'WIFI',
-    'timeout': 300, // in seconds
-    'prefill': {
-      'contact': '9012345678',
-      'email': 'dummy@gmail.com',
-    },
-  };
+  Map<String, dynamic> options = {};
+
+  var phone = UserDataHandler.getUserPhone();
+  var email = UserDataHandler.getUserEmail();
 
   RazorpayPayment({required String orderId}) {
     _razorpay.on(Razorpay.EVENT_PAYMENT_SUCCESS, _handlePaymentSuccess);
     _razorpay.on(Razorpay.EVENT_PAYMENT_ERROR, _handlePaymentError);
     _razorpay.on(Razorpay.EVENT_EXTERNAL_WALLET, _handleExternalWallet);
+
+    options = {
+      'key': 'HqtzkMv1vxs2f8',
+      'amount': 1000, //in the smallest currency sub-unit.
+      'name': 'Lokal',
+      'order_id': orderId, // Generate order_id using Orders API
+      'description': 'WIFI',
+      'timeout': 300, // in seconds
+      'prefill': {
+        'contact': phone,
+        'email': email,
+      },
+    };
   }
 
   void dispose() {
