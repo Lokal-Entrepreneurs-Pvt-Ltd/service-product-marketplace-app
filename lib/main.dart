1c3c4f963e7693a807fc3ace9ae62d972b2957d3
Mano Vikram <manovik18@gmail.com>
2023-03-13 12:36:02 +0530

    Added Razorpay SDK!

--- lib/main.dart
+++ lib/main.dart
@@ -147,7 +147,7 @@ class _LokalAppState extends State<LokalApp> {
           // "/": (context) => UikHome().page,
 
           "/": (context) => UserDataHandler.getUserToken().isEmpty ?  OnboardingScreen() : UikBottomNavigationBar(),
-        //  "/": (context) => UikAddAddressScreen().page,
+        //  "/": (context) => UikPaymentDetailsScreen().page,
           ScreenRoutes.homeScreen: (context) => const UikHomeWrapper(),
           ScreenRoutes.catalogueScreen: (context) => UikCatalogScreen().page,
           ScreenRoutes.productScreen: (context) => UikProductPage().page,
