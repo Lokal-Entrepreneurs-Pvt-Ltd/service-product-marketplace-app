4af136736e8a01c34f2bc9664d87ccedfd122f4f
Mano Vikram <manovik18@gmail.com>
2023-03-31 15:10:56 +0530

    BTS location feasibility - unifinished - problem with getDistrict screen - getState working properly

--- lib/pages/UikDistrictList.dart
+++ lib/pages/UikDistrictList.dart
@@ -32,25 +32,29 @@ class UikDistrictList extends StandardPage {
 
   @override
   dynamic getData() {
-    return ApiRepository.getDistricts;
-    // return getMockedApiResponse;
+    // return ApiRepository.getDistricts({
+    //   "stateCode": stateCode,
+    // });
+    return getMockedApiResponse({
+      "stateCode": stateCode,
+    });
   }
 
   void onStateListScreenTapAction(UikAction uikAction) {
     print(uikAction.tap.type);
     switch (uikAction.tap.type) {
-      case "SELECT_CITY":
+      case "SELECT_DISTRICT":
         {
           print("Inside Select City");
           print(uikAction.tap.data.value);
           selectedValue = int.parse(uikAction.tap.data.value!);
         }
         break;
-      case "CITY_SELECTED":
+      case "SUBMIT_FORM":
         {
           print(selectedValue);
           var context = NavigationService.navigatorKey.currentContext;
-          Navigator.of(context!).pop(selectedValue);
+          Navigator.maybePop(context!, selectedValue);
         }
         break;
       default:
@@ -73,14 +77,19 @@ Future<ApiResponse>? getMockedApiResponse(args) async {
     "id": "eb5f37b2-ca34-40a1-83ba-cb161eb55e6e",
   };
 
-  final response = await http.get(
-    // Uri.parse('https://demo8009892.mockable.io/statelist'),
-    Uri.parse('https://demo9060148.mockable.io/statelist'),
+  print(args);
+
+  final response = await http.post(
+    // Uri.parse('https://demo4081726.mockable.io/districtslist'),
+    Uri.parse(
+        'https://0fe2-2405-201-e029-5bc9-2e08-8f82-fde-9131.in.ngrok.io/isp/feasiblity/getDistrictForState'),
     headers: {
       "ngrok-skip-browser-warning": "value",
     },
+    body: args,
   );
 
+  print("Hellowww");
   print(response.body);
 
   if (response.statusCode == 200) {
