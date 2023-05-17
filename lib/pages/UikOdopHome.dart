092ff998ac81854618c7c0a1ac284ab88e9d4832
SJacob2000 <shubhamjacob0512@gmail.com>
2023-05-17 17:36:40 +0530

    Changes For the Odop home page

---
+++ lib/pages/UikOdopHome.dart
@@ -0,0 +1,89 @@
+import 'dart:convert';
+import 'package:flutter/widgets.dart';
+import 'package:lokal/screen_routes.dart';
+import 'package:lokal/utils/NavigationUtils.dart';
+import 'package:lokal/utils/deeplink_handler.dart';
+import 'package:lokal/utils/network/ApiRepository.dart';
+import 'package:lokal/utils/network/retrofit/api_routes.dart';
+import 'package:ui_sdk/StandardPage.dart';
+import 'package:http/http.dart' as http;
+import 'package:ui_sdk/props/ApiResponse.dart';
+import 'package:ui_sdk/props/UikAction.dart';
+import '../constants.dart';
+import '../main.dart';
+import '../actions.dart';
+
+class UikOdopHome extends StandardPage {
+  @override
+  Set<String?> getActions() {
+    Set<String?> actionList = Set();
+    actionList.add(UIK_ACTION.CHECK_FEASIBILITY);
+    actionList.add(UIK_ACTION.BACK_PRESSED);
+    return actionList;
+  }
+
+  @override
+  dynamic getData() {
+    // return ApiRepository.getIspHomescreen;
+    return getMockedApiResponse;
+  }
+
+  void ispHomeScreenTapAction(UikAction uikAction) {
+    print(uikAction.tap.type);
+    switch (uikAction.tap.type) {
+      case UIK_ACTION.CHECK_FEASIBILITY:
+        submitCheckFeasibility(uikAction);
+        break;
+      case UIK_ACTION.BACK_PRESSED:
+        NavigationUtils.pop();
+        break;
+      default:
+    }
+  }
+
+  @override
+  getPageCallBackForAction() {
+    return ispHomeScreenTapAction;
+  }
+
+  @override
+  getPageContext() {
+    return UikOdopHome;
+  }
+
+  @override
+  getConstructorArgs() {
+    return {};
+  }
+}
+
+void submitCheckFeasibility(UikAction uikAction) async {
+  // NavigationUtils.openPage(uikAction);
+  final BuildContext context = NavigationService.navigatorKey.currentContext!;
+
+  Navigator.pushNamed(
+    context,
+    ScreenRoutes.btsLocationFeasibility,
+  );
+}
+
+Future<ApiResponse> getMockedApiResponse(args) async {
+  final queryParameter = {
+    "id": "eb5f37b2-ca34-40a1-83ba-cb161eb55e6e",
+  };
+  print("entering lavesh");
+  final response = await http.get(
+    Uri.parse('http://demo7588460.mockable.io/test'),
+    headers: {
+      "ngrok-skip-browser-warning": "value",
+    },
+  );
+
+  print(response.body);
+
+  if (response.statusCode == 200) {
+    return ApiResponse.fromJson(jsonDecode(response.body));
+  } else {
+    throw Exception('Failed to load album');
+  }
+}
