8ad819e5dcd726a661d4a272b95c89edaa30635f
anupamsingh-digia <digia0211>
2023-07-03 15:08:56 +0530

    ios release 1.0 fixes

--- windows/flutter/generated_plugin_registrant.cc
+++ windows/flutter/generated_plugin_registrant.cc
@@ -6,7 +6,6 @@
 
 #include "generated_plugin_registrant.h"
 
-#include <awesome_notifications/awesome_notifications_plugin_c_api.h>
 #include <firebase_core/firebase_core_plugin_c_api.h>
 #include <geolocator_windows/geolocator_windows.h>
 #include <platform_device_id_windows/platform_device_id_windows_plugin.h>
@@ -14,8 +13,6 @@
 #include <url_launcher_windows/url_launcher_windows.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
-  AwesomeNotificationsPluginCApiRegisterWithRegistrar(
-      registry->GetRegistrarForPlugin("AwesomeNotificationsPluginCApi"));
   FirebaseCorePluginCApiRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("FirebaseCorePluginCApi"));
   GeolocatorWindowsRegisterWithRegistrar(
