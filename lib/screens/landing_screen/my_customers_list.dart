272f40810f81906aaa7f142f9f60f5806e30f334
anupamsingh-digia <digia0211>
2023-09-07 15:49:31 +0530

    fixed tab color change on tap and scroll

--- lib/screens/landing_screen/my_customers_list.dart
+++ lib/screens/landing_screen/my_customers_list.dart
@@ -11,7 +11,6 @@ import 'package:ui_sdk/props/UikContainerTextProps.dart';
 
 import '../../utils/network/ApiRepository.dart';
 
-
 class Sl_MyCustomersList extends StatefulWidget {
   const Sl_MyCustomersList({super.key});
 
@@ -24,28 +23,23 @@ class _Sl_DetailsPageState extends State<Sl_MyCustomersList>
   late final TabController _tabController;
   late final AutoScrollController _scrollController;
   int _currentTabNumber = 0;
-  List<Map<String, dynamic>> _customerListDataStore = []; // Store customer data
+  List<Map<String, dynamic>> _customerListDataStore = [];
   List<dynamic> _customerWidgets = [];
+  bool _isLoading = true;
+  bool _showAddCustomerButton = false; // Initialize to false
+
   @override
   void initState() {
     _tabController = TabController(length: 5, vsync: this);
     _scrollController = AutoScrollController();
-    _fetchCustomerData(); // Call the API to fetch customer data
+    _fetchCustomerData();
     super.initState();
   }
 
-  // Function to fetch customer data from the API
   Future<void> _fetchCustomerData() async {
-    // final dynamic args = ModalRoute.of(context)!.settings.arguments ?? {};
-    final response = await ApiRepository.getAllCustomerForUserService({ "serviceId": "16"});
+    final response = await ApiRepository.getAllCustomerForUserService({"serviceId": "16"});
     if (response.isSuccess!) {
-
-      // Extract customer data from the API response
       _customerWidgets = response.data['widgets'] as List<dynamic>;
-      // final customers = customerWidgets
-      //     .where((widget) => widget['uiType'] == 'UikListItemCustomer')
-      //     .toList();
-
       final dataStore = response.data['dataStore'] as Map<String, dynamic>;
       final customerDataList = _customerWidgets.map((customer) {
         final id = customer['id'] as String;
@@ -54,153 +48,109 @@ class _Sl_DetailsPageState extends State<Sl_MyCustomersList>
 
       setState(() {
         _customerListDataStore = customerDataList;
+        _isLoading = false;
+        _showAddCustomerButton = true; // Set to true after response is received
       });
     } else {
-      throw Exception('Failed to load customer data');
+      setState(() {
+        _isLoading = false;
+        _showAddCustomerButton = false; // Set to false on error
+      });
+      // Handle API error here and display an error message to the user
     }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
+      body: _isLoading
+          ? Center(
+        child: CircularProgressIndicator(
+          valueColor: AlwaysStoppedAnimation<Color>(Colors.yellow), // Set the progress indicator color to yellow
+        ),
+      )
+          : Container(
         child: ListView.builder(
           shrinkWrap: true,
-          itemCount: _customerListDataStore.length, // Use the length of customer data
+          itemCount: _customerListDataStore.length,
           physics: const BouncingScrollPhysics(
               parent: AlwaysScrollableScrollPhysics()),
           controller: _scrollController,
           itemBuilder: (ctx, index) {
             final customer = _customerListDataStore[index];
             final uiType = _customerWidgets[index]['uiType'] as String;
-         if (uiType == 'UikListItemCustomer') {
-            return ListTile(
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (ctx) => Sl_CustomerDetails(),
-                  ),
-                );
-              },
-              leading: CircleAvatar(
-                backgroundColor: Colors.yellow,
-                child: Text("S"),
-              ),
-              title: Text(
-                customer['name']['text'] ?? '',
-                style: GoogleFonts.poppins(
-                  fontSize: 14,
-                  fontWeight: FontWeight.w500,
-                  color: Colors.black,
+            if (uiType == 'UikListItemCustomer') {
+              return ListTile(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (ctx) => Sl_CustomerDetails(),
+                    ),
+                  );
+                },
+                leading: CircleAvatar(
+                  backgroundColor: Colors.yellow,
+                  child: Text("S"),
                 ),
-              ),
-              subtitle: Text(
-                customer['phone']['text'] ?? '',
-                style: GoogleFonts.poppins(
+                title: Text(
+                  customer['name']['text'] ?? '',
+                  style: GoogleFonts.poppins(
                     fontSize: 14,
-                    fontWeight: FontWeight.w400,
-                    color: Colors.grey),
-              ),
-              trailing: Text(
-                customer['date']['text'] ?? '',
-                style: GoogleFonts.poppins(
-                    fontSize: 14,
-                    fontWeight: FontWeight.w400,
-                    color: Colors.grey),
-              ),
-            );}
-         else if (uiType == 'UikContainerText') {
-           return Container(
-             padding: EdgeInsets.all(8.0),
-             child: Text(
-               customer['text'] ?? '',
-               style: GoogleFonts.poppins(
-                 fontSize: 12,
-                 fontWeight: FontWeight.w600,
-                 color: Colors.black,
-               ),
-             ),
-           );
-         } else {
-           // Handle other uiTypes as needed
-           return SizedBox(); // Placeholder for other uiTypes
-         }
+                    fontWeight: FontWeight.w500,
+                    color: Colors.black,
+                  ),
+                ),
+                subtitle: Text(
+                  customer['phone']['text'] ?? '',
+                  style: GoogleFonts.poppins(
+                      fontSize: 14,
+                      fontWeight: FontWeight.w400,
+                      color: Colors.grey),
+                ),
+                trailing: Text(
+                  customer['date']['text'] ?? '',
+                  style: GoogleFonts.poppins(
+                      fontSize: 14,
+                      fontWeight: FontWeight.w400,
+                      color: Colors.grey),
+                ),
+              );
+            } else if (uiType == 'UikContainerText') {
+              return Container(
+                padding: EdgeInsets.all(8.0),
+                child: Text(
+                  customer['text'] ?? '',
+                  style: GoogleFonts.poppins(
+                    fontSize: 12,
+                    fontWeight: FontWeight.w600,
+                    color: Colors.black,
+                  ),
+                ),
+              );
+            } else {
+              return SizedBox();
+            }
           },
         ),
       ),
       persistentFooterButtons: [
-        Container(
-          child: InkWell(
-            onTap: () {
-              NavigationUtils.openScreen(
-                  ScreenRoutes.addUserServiceCustomer);
-            },
-            child: UikButton(
-              text: "Add Customer",
-              textColor: Colors.black,
-              textSize: 16.0,
-              textWeight: FontWeight.w500,
-            ),
-          ),
-        ),
-      ],
-    );
-  }}
-
-class ArrowDetailsWidget extends StatelessWidget {
-  final String point;
-  const ArrowDetailsWidget(
-      {super.key, required this.point, this.fontSize = 12});
-  final double fontSize;
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 2),
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Icon(
-            Icons.chevron_right,
-            size: fontSize,
-          ),
-          Expanded(
-            child: Text(
-              point,
-              textAlign: TextAlign.start,
-              style: GoogleFonts.poppins(
-                fontSize: fontSize,
-                fontWeight: FontWeight.w400,
+        if (_showAddCustomerButton) // Show the button only when _showAddCustomerButton is true
+          Container(
+            child: InkWell(
+              onTap: () {
+                NavigationUtils.openScreen(
+                    ScreenRoutes.addUserServiceCustomer);
+              },
+              child: UikButton(
+                text: "Add Customer",
+                textColor: Colors.black,
+                textSize: 16.0,
+                textWeight: FontWeight.w500,
               ),
             ),
           ),
-        ],
-      ),
-    );
-  }
-}
-
-class TabBarDetailsElement extends StatelessWidget {
-  final String text;
-  final int index;
-  bool isSelected;
-  TabBarDetailsElement({
-    super.key,
-    required this.text,
-    required this.index,
-    this.isSelected = false,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Tab(
-      child: Text(
-        text,
-        style: TextStyle(
-          color: isSelected ? Colors.white : Colors.black,
-          fontWeight: FontWeight.w600,
-        ),
-      ),
+      ],
     );
   }
 }
