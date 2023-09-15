ca6d2a4a82d68f755b1963707cb125e304c7cb25
anupamsingh-digia <digia0211>
2023-09-16 02:42:21 +0530

    updated environment

--- lib/configs/environment_data_handler.dart
+++ lib/configs/environment_data_handler.dart
@@ -21,6 +21,6 @@ class EnvironmentDataHandler {
 // Set your url here
   static String  getLocalBaseUrl() {
     return PreferenceUtils.getString(
-        LOCAL_BASE_URL,"https://b1cb-103-48-109-107.ngrok-free.app");
+        LOCAL_BASE_URL,"https://prod.localee.co.in/api");
   }
 }
