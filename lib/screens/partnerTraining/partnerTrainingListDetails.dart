2a7a0280380444f0ba1a1f7477bb8308b86a7618
anupamsingh-digia <digia0211>
2023-09-26 19:21:50 +0530

    fixes code of material tile

--- lib/screens/partnerTraining/partnerTrainingListDetails.dart
+++ lib/screens/partnerTraining/partnerTrainingListDetails.dart
@@ -1,11 +1,9 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
-import 'package:lokal/utils/UiUtils/UiUtils.dart';
 import 'package:ui_sdk/props/ApiResponse.dart';
 import '../../Widgets/UikMaterialListTile/UikMaterialListTile.dart';
 import '../../utils/network/ApiRepository.dart';
 import 'package:google_fonts/google_fonts.dart';
+
 
 class PartnerTrainingListDetailsWidget extends StatefulWidget {
   final dynamic args;
@@ -13,30 +11,48 @@ class PartnerTrainingListDetailsWidget extends StatefulWidget {
 
   @override
   State<PartnerTrainingListDetailsWidget> createState() =>
-      _ServiceLandingScreenState();
+      _PartnerTrainingListDetailsWidgetState(args: args);
 }
 
-class _ServiceLandingScreenState
+class _PartnerTrainingListDetailsWidgetState
     extends State<PartnerTrainingListDetailsWidget> {
   late Future<ApiResponse> _trainingData;
   late dynamic args;
 
-  @override
-  void didChangeDependencies() {
-    args = ModalRoute.of(context)?.settings.arguments ?? widget.args;
-
-    // var enc = JsonEncoder.withIndent("    ");
-    // var js = enc.convert(args);
-    // debugPrint(js);
-
-    _trainingData = ApiRepository.getServiceTabsScreen(args);
-
-    super.didChangeDependencies();
-  }
+  _PartnerTrainingListDetailsWidgetState({this.args});
 
   @override
   void initState() {
     super.initState();
+    _trainingData = ApiRepository.getAcademyDataByType(args);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: _trainingData,
+      builder: (context, AsyncSnapshot<ApiResponse> snap) {
+        if (snap.connectionState == ConnectionState.waiting) {
+          return _buildLoadingIndicator();
+        } else if (snap.hasError) {
+          return Center(
+            child: Text("Something went wrong\t ${snap.error}"),
+          );
+        }
+        dynamic data = snap.data?.data;
+        return SingleChildScrollView(
+          physics:
+          BouncingScrollPhysics(parent: AlwaysScrollableScrollPhysics()),
+          child: Column(
+            children: [
+              _buildHeaderImage(data),
+              const SizedBox(height: 8),
+              ..._buildMaterialListTiles(data),
+            ],
+          ),
+        );
+      },
+    );
   }
 
   Widget _buildLoadingIndicator() {
@@ -47,93 +63,71 @@ class _ServiceLandingScreenState
     );
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return FutureBuilder(
-      future: null, // Use the stored future
-      builder: (context, AsyncSnapshot<ApiResponse> snap) {
-        if (snap.connectionState == ConnectionState.waiting) {
-          return _buildLoadingIndicator();
-        } else if (snap.hasError) {
-          return Center(
-            child: Text("Something went wrong\t ${snap.error}"),
-          );
-        }
-        // dynamic data = snap.data?.data;
-        dynamic data = samplePartnerTrainingData["data"]['response'];
-
-        return SingleChildScrollView(
-          physics:
-              BouncingScrollPhysics(parent: AlwaysScrollableScrollPhysics()),
-          child: Column(
-            children: [
-              Padding(
-                padding:
-                    const EdgeInsets.symmetric(vertical: 15.0, horizontal: 8),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    ClipRRect(
-                      borderRadius: BorderRadius.circular(8),
-                      child: Image.network(
-                        data["image"],
-                        fit: BoxFit.contain,
-                      ),
-                    ),
-                    const SizedBox(height: 15),
-                    Container(
-                      margin: const EdgeInsets.symmetric(horizontal: 0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            '${data["title"]}',
-                            style: GoogleFonts.poppins(
-                              fontWeight: FontWeight.w500,
-                              fontSize: 18,
-                            ),
-                          ),
-                          const SizedBox(height: 5),
-                          Text(
-                            '${data["description"]}',
-                            style: GoogleFonts.poppins(
-                              fontWeight: FontWeight.w300,
-                              fontSize: 12,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
+  Widget _buildHeaderImage(dynamic data) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 15.0, horizontal: 8),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(8),
+            child: Image.network(
+              data["image"],
+              fit: BoxFit.contain,
+            ),
+          ),
+          const SizedBox(height: 15),
+          Container(
+            margin: const EdgeInsets.symmetric(horizontal: 0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  '${data["title"]}',
+                  style: GoogleFonts.poppins(
+                    fontWeight: FontWeight.w500,
+                    fontSize: 18,
+                  ),
                 ),
-              ),
-              const SizedBox(height: 8),
-              ...List<Widget>.generate(
-                (data["materials"] as List).length,
-                (index) {
-                  dynamic tileData = data["materials"][index];
+                const SizedBox(height: 5),
+                Text(
+                  '${data["description"]}',
+                  style: GoogleFonts.poppins(
+                    fontWeight: FontWeight.w300,
+                    fontSize: 12,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 
-                  return Card(
-                    elevation: 0.0,
-                    margin: const EdgeInsets.symmetric(vertical: 5),
-                    child: Container(
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 10, vertical: 10),
-                      child: TrainingCourseMaterialListTile(
-                        tileData: tileData,
-                      ),
-                    ),
-                  );
-                },
-              ).toList(),
-            ],
+  List<Widget> _buildMaterialListTiles(dynamic data) {
+    return List<Widget>.generate(
+      (data["materials"] as List).length,
+          (index) {
+        dynamic tileData = data["materials"][index];
+
+        return Card(
+          elevation: 0.0,
+          margin: const EdgeInsets.symmetric(vertical: 5),
+          child: Container(
+            padding:
+            const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+            child: TrainingCourseMaterialListTile(
+              tileData: tileData,
+            ),
           ),
         );
       },
-    );
+    ).toList();
   }
 }
+
 
 dynamic samplePartnerTrainingData = {
   "isSuccess": true,
@@ -291,133 +285,3 @@ dynamic samplePartnerTrainingData = {
   }
 };
 
-
-/*
-{
-    "isSuccess": true,
-    "data": {
-        "response": {
-            "image": "https://www.poornima.edu.in/wp-content/uploads/2020/10/Central-Library.jpg",
-            "title": "Basics of Computer Fundamentals",
-            "description": "Computer Fundamentals",
-            "materials": [
-                {
-                    "title": "Web Developement",
-                    "description": "Learn the basics of Web Dev",
-                    "duration": "6 weeks",
-                    "instructor": "David Johnson",
-                    "thumbnail": "https://www.pngitem.com/pimgs/m/255-2550411_404-error-images-free-png-transparent-png.png",
-                    "logo": "https://www.pngitem.com/pimgs/m/255-2550411_404-error-images-free-png-transparent-png.png",
-                    "action": {
-                        "type": "OPEN_PAGE",
-                        "data": {
-                            "url": "https://www.youtube.com"
-                        }
-                    }
-                },
-                {
-                    "title": "Learn Web Development and Get Hired in 2023",
-                    "description": "Learn Web Development and Get Hired in 2023 | Tanay Pratap Hindi",
-                    "duration": "12 weeks",
-                    "instructor": "Tanay Pradap",
-                    "thumbnail": "https://i.ytimg.com/vi/TLacepiMuqk/maxresdefault.jpg",
-                    "logo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRSrDmV9e39L-T1mHzgBPiD7FrlyVC8-UXSq3rcjnoviQ&s",
-                    "action": {
-                        "type": "OPEN_PAGE",
-                        "data": {
-                            "url": "https://www.youtube.com/watch?v=y2omP4c7WKw"
-                        }
-                    }
-                }
-            ]
-        }
-    }
-}
-
-
----------
-
-OPEN_VIDEO
-
-{
-  "material_data": {
-    "title": "How to Become Tata New agent!",
-    "instructor": "Acharya Prashant",
-    "dateTime": "30 minutes",
-    "thumbnail": "https://www.pngitem.com/pimgs/m/255-2550411_404-error-images-free-png-transparent-png.png",
-     "logo": "https://www.pngitem.com/pimgs/m/255-2550411_404-error-images-free-png-transparent-png.png",
-     "action": {
-      "type": "OPEN_VIDEO",
-        "data": {
-                "url": "https://www.youtube.com/watch?v=c_6jv5X-2h4"
-        }
-  }
-  },
-  "material_type": "VIDEO",
-  "service_id": 2,
-  "academy_id": 5
-}
-
-
-------------
-COURSE
-{
-  "material_data": {
-    "title": "How to Become Tata New agent!",
-    "instructor": "Acharya Prashant",
-    "thumbnail": "https://www.pngitem.com/pimgs/m/255-2550411_404-error-images-free-png-transparent-png.png",
-     "logo": "https://www.pngitem.com/pimgs/m/255-2550411_404-error-images-free-png-transparent-png.png",
-     "action": {
-      "type": "OPEN_PAGE",
-        "data": {
-                "url": "https://lokalcompanyin/trainingCourse?training_courseId=1"
-        }
-  }
-  },
-  "material_type": "COURSE",
-  "service_id": 2,
-  "academy_id": 5
-}
-
-
----------
-LIVE
-{
-  "material_data": {
-    "title": "How to Become Tata New agent!",
-    "instructor": "Acharya Prashant",
-    "thumbnail": "https://www.pngitem.com/pimgs/m/255-2550411_404-error-images-free-png-transparent-png.png",
-     "logo": "https://www.pngitem.com/pimgs/m/255-2550411_404-error-images-free-png-transparent-png.png",
-     "action": {
-      "type": "OPEN_WEB",
-        "data": {
-                "url": "https://meet.google.com/zqz-frvv-fdw"
-        }
-  }
-  },
-  "material_type": "LIVE",
-  "service_id": 2,
-  "academy_id": 5
-}
-
-
----------
-DOCUMENT
-{
-  "material_data": {
-    "title": "How to Become Tata New agent!",
-    "instructor": "Acharya Prashant",
-    "thumbnail": "https://www.pngitem.com/pimgs/m/255-2550411_404-error-images-free-png-transparent-png.png",
-     "logo": "https://www.pngitem.com/pimgs/m/255-2550411_404-error-images-free-png-transparent-png.png",
-     "action": {
-      "type": "OPEN_WEB",
-        "data": {
-                "url": "https://docs.google.com/document/d/1W6wfYW69wOvsYfzN3QaG6AtsSq6TKbWrZcXnOIP3B8I/edit#heading=h.n2j26c77qltl"
-        }
-  }
-  },
-  "material_type": "DOCUMENT",
-  "service_id": 2,
-  "academy_id": 5
-}
-*/
