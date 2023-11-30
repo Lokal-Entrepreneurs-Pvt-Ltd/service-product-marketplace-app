f9c2b530a61c238fcb18875ca013c04acef14d47
Ramsthr <ramsthr38@gmail.com>
2023-11-30 23:00:20 +0530

    Added version in account section footer

--- lib/screens/myAccount/myAccountPageWrapper.dart
+++ lib/screens/myAccount/myAccountPageWrapper.dart
@@ -1,15 +1,24 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
-import 'package:lokal/pages/UikMyAccountScreen.dart';
 import 'package:package_info_plus/package_info_plus.dart';
 
+import 'package:lokal/pages/UikMyAccountScreen.dart';
+
 class MyAccountWrapper extends StatefulWidget {
-  const MyAccountWrapper({super.key});
+  StatefulWidget page;
+  MyAccountWrapper({
+    Key? key,
+    required this.page,
+  }) : super(key: key);
 
   @override
-  State<MyAccountWrapper> createState() => _MyAccountWrapperState();
+  State<MyAccountWrapper> createState() => _MyAccountWrapperState(page);
 }
 
 class _MyAccountWrapperState extends State<MyAccountWrapper> {
+  final StatefulWidget page;
+  _MyAccountWrapperState(this.page);
+
   Future<String?> _initPackageInfo() async {
     final packageInfo = await PackageInfo.fromPlatform();
     return packageInfo.version;
@@ -25,26 +34,36 @@ class _MyAccountWrapperState extends State<MyAccountWrapper> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          UikMyAccountScreen().page,
-          Expanded(child: Container()),
-          FutureBuilder(
-              future: _initPackageInfo(),
-              builder: (ctx, res) {
-                if (res.hasData) {
-                  return Container(
-                    width: double.infinity,
-                    color: Colors.grey,
-                    child: Text(
-                      "Version: ${res.data!}",
-                    ),
-                  );
-                } else {
-                  return Container();
-                }
-              }),
-        ],
+      body: SafeArea(
+        child: Column(
+          children: [
+            //UikMyAccountScreen().page,
+            SizedBox(
+              child: page,
+              height: MediaQuery.of(context).size.height * 0.9,
+            ),
+            Expanded(child: Container()),
+            FutureBuilder(
+                future: _initPackageInfo(),
+                builder: (ctx, res) {
+                  if (res.hasData) {
+                    return Padding(
+                      padding: const EdgeInsets.only(top: 10),
+                      child: Container(
+                        width: double.infinity,
+                        color: Colors.grey,
+                        child: Text(
+                          "Version: ${res.data!}",
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                    );
+                  } else {
+                    return Container();
+                  }
+                }),
+          ],
+        ),
       ),
     );
   }
