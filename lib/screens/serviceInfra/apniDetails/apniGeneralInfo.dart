de283098932d4bdc8e3fdcf596dc7dc7b2e83319
Ramsthr <ramsthr38@gmail.com>
2024-02-19 19:54:15 +0530

    seperated modelbottomsheet and added State And District apis

--- lib/screens/serviceInfra/apniDetails/apniGeneralInfo.dart
+++ lib/screens/serviceInfra/apniDetails/apniGeneralInfo.dart
@@ -4,8 +4,11 @@ import 'package:lokal/constants/strings.dart';
 import 'package:lokal/screen_routes.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:lokal/utils/NavigationUtils.dart';
+import 'package:lokal/utils/UiUtils/UiUtils.dart';
+import 'package:lokal/utils/location/State_and_district.dart';
 import 'package:lokal/utils/uik_color.dart';
 import 'package:lokal/widgets/UikButton/UikButton.dart';
+import 'package:lokal/widgets/modalBottomSheet.dart';
 import 'package:lokal/widgets/selectabletext.dart';
 import 'package:lokal/widgets/textInputContainer.dart';
 import 'package:ui_sdk/getWidgets/colors/UikColors.dart';
@@ -20,26 +23,20 @@ class ApniGeneralInfo extends StatefulWidget {
 
 enum IndexType { bike, bank }
 
-enum LocationType { state, district, city }
-
 class _ApniGeneralInfoState extends State<ApniGeneralInfo> {
   Future<Map<String, dynamic>?>? _futureData;
   int bikeIndex = -1;
   int bankIndex = -1;
-  String selectedState = "";
-  String selectedDistrict = "";
-  String selectedCity = "";
   String home = "";
   String road = "";
 
   List<String> bike = ["Yes", "No"];
-  List<String> stateList = ["Rajasthan", "Pakistan", "Mumbai", "Bangalore"];
-  List<String> districtList = [
-    "Yamla",
-    "Pagla",
-    "Deewana",
-  ];
+  StateDataList stateDataList = StateDataList(args: {});
+  int stateIndex = -1;
+  DistrictDataList districtDataList = DistrictDataList();
+  int districtIndex = -1;
   List<String> villageList = ["Rajasthan", "Pakistan", "Mumbai", "Bangalore"];
+  int villageIndex = -1;
 
   bool isUpdating = false; // Added isUpdating variable
 
@@ -84,12 +81,87 @@ class _ApniGeneralInfoState extends State<ApniGeneralInfo> {
                   updateSelectedIndex(index, IndexType.bank);
                 }),
                 buildTitle("Permanent Addresses", 16, FontWeight.w500),
-                builLocationsField(
-                    context, stateList, "State", LocationType.state),
-                builLocationsField(
-                    context, districtList, "District", LocationType.district),
-                builLocationsField(
-                    context, villageList, "Village", LocationType.city),
+                GestureDetector(
+                  onTap: () async {
+                    int? result = await Bottomsheets.showLocationDialog(
+                      context,
+                      stateDataList.stateNameList,
+                      "State",
+                      () async {
+                        await stateDataList.initialize();
+                        return DataForFunction(
+                            index: stateIndex,
+                            list: stateDataList.stateNameList);
+                      },
+                    );
+                    if (result != null && result >= 0) {
+                      setState(() {
+                        stateIndex = result;
+                      });
+                    }
+                  },
+                  child: builbottomsheedtfield(
+                      "State",
+                      (stateIndex != -1)
+                          ? stateDataList.stateNameList[stateIndex]
+                          : ""),
+                ),
+                GestureDetector(
+                  onTap: () async {
+                    if (stateIndex != -1) {
+                      int? result = await Bottomsheets.showLocationDialog(
+                        context,
+                        stateDataList.stateNameList,
+                        "District",
+                        () async {
+                          await districtDataList
+                              .initialize(args: {"stateCode": stateIndex});
+                          return DataForFunction(
+                              index: districtIndex,
+                              list: districtDataList.districtNameList);
+                        },
+                      );
+                      print(districtDataList.districtNameList);
+                      if (result != null && result >= 0) {
+                        setState(() {
+                          districtIndex = result;
+                        });
+                      }
+                    } else {
+                      UiUtils.showToast("Please Select State");
+                    }
+                  },
+                  child: builbottomsheedtfield(
+                      "District",
+                      (districtIndex != -1)
+                          ? districtDataList.districtNameList[districtIndex]
+                          : ""),
+                ),
+                GestureDetector(
+                  onTap: () async {
+                    if (districtIndex != -1) {
+                      int? result = await Bottomsheets.showLocationDialog(
+                        context,
+                        stateDataList.stateNameList,
+                        "District",
+                        () async {
+                          Future.delayed(const Duration(milliseconds: 500));
+                          return DataForFunction(
+                              index: villageIndex, list: villageList);
+                        },
+                      );
+                      if (result != null && result >= 0) {
+                        setState(() {
+                          villageIndex = result;
+                        });
+                      }
+                    } else {
+                      UiUtils.showToast("Please Select District");
+                    }
+                  },
+                  child: builbottomsheedtfield("Village",
+                      (villageIndex != -1) ? villageList[villageIndex] : ""),
+                ),
                 TextInputContainer(
                   fieldName: "Home No, Building Name",
                   hint: "Type your address",
@@ -117,6 +189,45 @@ class _ApniGeneralInfoState extends State<ApniGeneralInfo> {
     );
   }
 
+  Widget builbottomsheedtfield(String name, String selectedname) {
+    return Container(
+      margin: EdgeInsets.only(bottom: 12),
+      padding: EdgeInsets.only(top: 9.5, left: 16, right: 16, bottom: 9.5),
+      height: 64,
+      decoration: BoxDecoration(
+        color: ("#F5F5F5").toColor(),
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                name,
+                textAlign: TextAlign.start,
+                style: GoogleFonts.poppins(
+                  fontSize: 14,
+                  fontWeight: FontWeight.w400,
+                  color: ("#9E9E9E").toColor(),
+                ),
+              ),
+              (selectedname.isNotEmpty)
+                  ? Text(selectedname,
+                      style: GoogleFonts.poppins(
+                          fontSize: 16, fontWeight: FontWeight.w400))
+                  : Container()
+            ],
+          ),
+          SvgPicture.network(
+              "https://storage.googleapis.com/lokal-app-38e9f.appspot.com/service%2F1708195274263-chevron-down.svg"),
+        ],
+      ),
+    );
+  }
+
   Widget buildSelectable(
       List<String> list, int selectedIndex, void Function(int) onTap) {
     return Wrap(
@@ -134,158 +245,14 @@ class _ApniGeneralInfoState extends State<ApniGeneralInfo> {
     );
   }
 
-  Widget builLocationsField(
-      BuildContext context, List<String> list, String name, LocationType type) {
-    return GestureDetector(
-      onTap: () {
-        _showLocationDialog(context, list, type);
-      },
-      child: Container(
-        margin: EdgeInsets.only(bottom: 12),
-        padding: EdgeInsets.only(top: 9.5, left: 16, right: 16, bottom: 9.5),
-        height: 64,
-        decoration: BoxDecoration(
-          color: ("#F5F5F5").toColor(),
-          borderRadius: BorderRadius.circular(10),
-        ),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  name,
-                  textAlign: TextAlign.start,
-                  style: GoogleFonts.poppins(
-                    fontSize: 14,
-                    fontWeight: FontWeight.w400,
-                    color: ("#9E9E9E").toColor(),
-                  ),
-                ),
-                (getlocationtype(type, null).isNotEmpty)
-                    ? Text(getlocationtype(type, null),
-                        style: GoogleFonts.poppins(
-                            fontSize: 16, fontWeight: FontWeight.w400))
-                    : Container(),
-              ],
-            ),
-            SvgPicture.network(
-                "https://storage.googleapis.com/lokal-app-38e9f.appspot.com/service%2F1708195274263-chevron-down.svg"),
-          ],
-        ),
-      ),
-    );
-  }
-
-  String getlocationtype(LocationType type, String? string) {
-    switch (type) {
-      case LocationType.state:
-        if (string != null) {
-          selectedState = string;
-        }
-        return selectedState;
-      case LocationType.district:
-        if (string != null) {
-          selectedDistrict = string;
-        }
-        return selectedDistrict;
-      case LocationType.city:
-        if (string != null) {
-          selectedCity = string;
-        }
-        return selectedCity;
-    }
-  }
-
-  Future<void> _showLocationDialog(
-      BuildContext context, List<String> list, LocationType type) async {
-    return showModalBottomSheet<void>(
-      backgroundColor: UikColors.WHITE,
-      context: context,
-      builder: (BuildContext context) {
-        return Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    "Select Your Current State",
-                    style: GoogleFonts.poppins(
-                      fontSize: 20,
-                      fontWeight: FontWeight.w500,
-                    ),
-                  ),
-                  InkWell(
-                    child: Icon(Icons.dangerous_outlined),
-                    onTap: () {
-                      Navigator.of(context).pop();
-                    },
-                  )
-                ],
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 5),
-              child: TextFormField(
-                style: GoogleFonts.poppins(
-                    fontSize: 16, fontWeight: FontWeight.w400),
-                decoration: InputDecoration(
-                  hintText: "Search State",
-                  border: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.grey),
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  filled: true,
-                  fillColor: Colors.white,
-                ),
-                scribbleEnabled: false,
-                onTap: () => updateState(),
-                onFieldSubmitted: (_) => updateState(),
-              ),
-            ),
-            Expanded(
-              child: ListView.builder(
-                itemCount: list.length, // Change the itemCount as needed
-                itemBuilder: (BuildContext context, int index) {
-                  return _buildLocationItem(
-                      context, list[index], index % 2 == 0, type);
-                },
-              ),
-            ),
-          ],
-        );
-      },
-    );
-  }
-
-  Widget _buildLocationItem(
-      BuildContext context, String state, bool isLightGrey, LocationType type) {
-    return Container(
-      color: isLightGrey ? Colors.grey[200] : Colors.white,
-      child: ListTile(
-        title: Text(state),
-        onTap: () {
-          Navigator.of(context).pop();
-          setState(() {
-            getlocationtype(type, state);
-          });
-        },
-      ),
-    );
-  }
-
   double calculateProgress() {
     // List of completion status for each field
     List<bool> fieldCompletionStatus = [
       bikeIndex != -1,
       bankIndex != -1,
-      selectedState.isNotEmpty,
-      selectedDistrict.isNotEmpty,
-      selectedCity.isNotEmpty,
+      stateIndex != -1,
+      villageIndex != -1,
+      districtIndex != -1,
       home.isNotEmpty,
       road.isNotEmpty
     ];
