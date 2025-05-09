c8178476f263fbaf11ca2c929583aa318b717ceb
anupamsingh-digia <anupam.singh0211@gmail.com>
2025-05-09 21:30:44 +0530

    new app icons

--- lib/configs/prod_config.dart
+++ lib/configs/prod_config.dart
@@ -3,7 +3,7 @@ import 'base_config.dart';
 
 class ProdConfig implements BaseConfig {
   @override
-  String get BASE_URL => "http://45.79.120.146/api";
+  String get BASE_URL => "https://api.lokalcompany.in";
   @override
   String get razorPayKey => "rzp_live_C3kVhJN9E4m5TG";
 
